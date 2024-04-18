911fd9a25c6e45d9fcb9ddda4a848dc9783bc071
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 22:36:02 +0300

    in order to make the project a bit more interesting and challenging for me, i've decided that ill make it using the cuda runtime. since what im doing is basicly just rendering a 2d image, it makes perfect sense to use it, plus, i'm learning a facinating technology that i have never used before. in this commit i've changed the project type to cuda runtime in vs, and created a cuda function that turns a rgb pixel monochrome.

---
+++ PNG_to_ascii/PNG_to_ascii/kernel.cu
@@ -0,0 +1,50 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+extern "C" {
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+}
+
+using std::string;
+using std::cout;
+using std::endl;
+
+__global__ const string brightness = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_ + ~<>i!lI;:,\"^`'.";
+
+__global__ void monochrome(int width, int height, int* in, int* out) {
+	const int x = threadIdx.x;
+	const int y = threadIdx.y;
+
+	out[y * width + x] = (in[y * width + x + 0] + in[y * width + x + 1] + in[y * width + x + 2]) / 3;
+}
+
+
+int main(char** argv, int argc) {
+	
+	int x, y, n;
+	unsigned char* data = stbi_load(argv[1], &x, &y, &n, 3);
+
+	if (!(data != nullptr && x > 0 && y > 0)) return -1;
+	if (n != 3) return -2;
+	s
+	unsigned char* mono = new unsigned char[y * x];
+
+	string* output;
+
+	
+
+	stbi_image_free(data);
+
+	int height = y / 20;
+	int width = x / 20;
+
+
+
+}
