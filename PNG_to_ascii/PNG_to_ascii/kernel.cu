1e3ef31799ba1d813da6389f85f74324fc75202c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 00:45:18 +0300

    currently trying to understand why the monochrome funtion returns an empty array when using cuda, i think it has something to do with the indexing of the threads.

--- PNG_to_ascii/PNG_to_ascii/kernel.cu
+++ PNG_to_ascii/PNG_to_ascii/kernel.cu
@@ -15,31 +15,63 @@ extern "C" {
 using std::string;
 using std::cout;
 using std::endl;
+using std::cin;
 
-__global__ const string brightness = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_ + ~<>i!lI;:,\"^`'.";
+const string brightness = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_ + ~<>i!lI;:,\"^`'.";
 
-__global__ void monochrome(int width, int height, int* in, int* out) {
+__global__ void monochrome(int* width, unsigned char* in, unsigned char* out) {
 	const int x = threadIdx.x;
 	const int y = threadIdx.y;
-
-	out[y * width + x] = (in[y * width + x + 0] + in[y * width + x + 1] + in[y * width + x + 2]) / 3;
+	out[y * *width + x] = ((int)in[y * *width + x + 0] + (int)in[y * *width + x + 1] + (int)in[y * *width + x + 2]) / 3;
 }
-
 
 int main(char** argv, int argc) {
 	
+	string path;
+	cin >> path;
+
 	int x, y, n;
-	unsigned char* data = stbi_load(argv[1], &x, &y, &n, 3);
+	unsigned char* data = stbi_load(path.c_str(), &x, &y, &n, 3);
+
+
+	unsigned char* cudaData = 0;
+	unsigned char* cudaMono = 0;
+	int* cudaWidth = 0;
+
 
 	if (!(data != nullptr && x > 0 && y > 0)) return -1;
-	if (n != 3) return -2;
-	s
-	unsigned char* mono = new unsigned char[y * x];
+	if (n < 3) return -2;
 
-	string* output;
+	cudaMalloc(&cudaWidth, sizeof(int));
+	cudaMalloc(&cudaData, x * y * 3);
+	cudaMalloc(&cudaMono, x * y);
+
+	cudaMemcpy(cudaData, data, x * y * 3, cudaMemcpyHostToDevice);
+	cudaMemcpy(cudaWidth, &x, sizeof(int), cudaMemcpyHostToDevice);
+
+	unsigned char* mono = new unsigned char[x * y];
 
 	
 
+
+	int numBlocks = 1;
+	dim3 threadsPerBlock(x, y);
+	monochrome<<<numBlocks, threadsPerBlock>>>(cudaWidth, cudaData, cudaMono);
+	cudaMemcpy(mono, cudaMono, x*y, cudaMemcpyDeviceToHost);
+
+
+	for (int i = 0; i < y; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			cout << (int)mono[i * x + j] << ", ";
+		}
+		cout << endl;
+	}
+
+	string* output;
+
+	delete mono;
 	stbi_image_free(data);
 
 	int height = y / 20;
